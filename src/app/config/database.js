7e7bae2270787ab49645d35f8e1da031bd5f0f19
unknown <Felipe Novaes>
2020-01-08 19:20:38 -0300

    Estrutura inicial da aplicação

---
+++ src/app/config/database.js
@@ -0,0 +1,3 @@
+module.exports = {
+  uri: 'mongodb+srv://mblabs:GOPQsd3yijFzCu9d@cluster0-a05wx.mongodb.net/mblabs?retryWrites=true&w=majority'
+}
---
+++ src/app/server.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const routes = require('./routes')
+const mongoose = require('mongoose')
+const databaseConfig = require('./config/database')
+
+class App {
+  constructor(){
+    this.express = express()
+
+    this.database()
+    this.middlewares()
+    this.routes()
+  }
+
+  database () {
+    mongoose.connect(databaseConfig.uri, 
+      { useNewUrlParser: true, 
+        useUnifiedTopology: true, 
+        useCreateIndex: true 
+      })
+  }
+  
+  middlewares () {
+    this.express.use(express.json())
+  }
+
+  routes () {
+    this.express.use(routes)
+  }
+}
+
+module.exports = new App().express
