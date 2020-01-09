6567c9913edde58c4043df3131c8a8714e4ddadb
unknown <Felipe Novaes>
2020-01-09 01:27:01 -0300

    Adicionando autenticação via token jwt

---
+++ src/app/config/auth.js
@@ -0,0 +1,4 @@
+module.exports = {
+  secret: 'desafioMbLabs' ,
+  ttl: 86400
+}
--- src/app/routes.js
+++ src/app/routes.js
@@ -2,15 +2,24 @@ const express = require('express')
 
 const routes = express.Router()
 
+const authMiddleaware = require('../app/middlewares/auth')
+
 const UserControler = require('./controllers/UserController')
 const EventControler = require('./controllers/EventController')
+const LoginController = require('./controllers/LoginController')
+const AttendenceController = require('./controllers/AttendenceController')
 
 routes.post('/users', UserControler.store)
+routes.post('/login', LoginController.store)
+
+routes.use(authMiddleaware)
 
 routes.post('/events', EventControler.store)
 routes.get('/events', EventControler.index)
 routes.get('/events/:id', EventControler.show)
 routes.delete('/events/:id', EventControler.destroy)
 routes.put('/events/:id', EventControler.update)
+
+routes.post('/attendence/:eventId', AttendenceController.store)
 
 module.exports = routes
